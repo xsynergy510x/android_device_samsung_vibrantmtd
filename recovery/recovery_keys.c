a7efad73a31777314372dd6d04901ed80251d832
Eduard Alfonso <xsynergy510x@gmail.com>
2014-09-12 16:58:45 -0700

    Bring vibrant inline with CM11

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -4,10 +4,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    return get_allow_toggle_display() && key_pressed[139] && key_pressed[KEY_VOLUMEUP];
-}
-
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
@@ -19,15 +15,13 @@ int device_handle_key(int key_code, int visible) {
             case 139: // MENU
                 return HIGHLIGHT_UP;
 
+            case KEY_POWER:
             case 158: // HOME
                 return SELECT_ITEM;
 
             case 102: // BACK
-                if (!get_allow_toggle_display())
-                    return GO_BACK;
+                return GO_BACK;
 
-            case KEY_POWER:
-                return SELECT_ITEM;
             default:
                 return NO_ACTION;
         }
